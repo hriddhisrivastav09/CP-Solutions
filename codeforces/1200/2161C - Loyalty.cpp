97e13d6fb1dd6c6389a909c8c61d9432c1c07359
Hriddhi Ranjan Srivastav <hriddhiranjansrivastav10b@gmail.com>
2026-08-23 19:28:30 +0530

    Add Loyalty (1200)

---
+++ codeforces/1200/2161C - Loyalty.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+using namespace std;
+using ll = long long ;
+
+ll gcd(ll a, ll b) {
+    while (b) {
+        ll t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pll = pair<ll, ll>;
+using vpll = vector<pll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define mx(v) *max_element(all(v))
+#define mn(v) *min_element(all(v))
+#define sum(v) accumulate(all(v), 0LL)
+
+#define rep(i, a, b) for(ll i = a; i < b; ++i)
+#define rrep(i, a, b) for(ll i = a; i >= b; --i)
+
+#define sz(x) ((ll)(x).size())
+#define print(x) cout << x << " " ;
+#define println(x) cout << x << '\n' ;
+#define yes cout << "Yes" << '\n';
+#define no cout << "No" << '\n';
+#define nl cout << '\n';
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+void solve(){
+
+    ll n , x ;
+    cin >> n >> x ;
+
+    vll a(n);
+    rep(i , 0 , n) cin >> a[i];
+
+    ll s = 0 ;
+    ll p = 0 ;
+
+    sort(all(a));
+
+    vll ans ;
+
+    ll i = 0 ;
+    ll j = n-1 ;
+
+    while(i <= j){
+        if((s+a[j])/x > s/x){
+            ans.pb(a[j]);
+            s+=a[j];
+            j--;
+        }
+        else{
+            ans.pb(a[i]);
+            s+=a[i];
+            i++;
+        }
+    }
+
+    s = 0 ;
+
+    rep(k,0,n){
+        ll w = s/x;
+        s+=ans[k];
+        ll t = s/x;
+        if(t>w){
+            p+=ans[k];
+        }
+    }
+
+    println(p);
+    rep(i,0,n){
+        print(ans[i]);
+    }
+    nl;
+
+
+
+}
+
+
+int main() {
+
+    fast_io;
+
+    ll t;
+    cin >> t;
+    while (t > 0) {
+        solve() ;
+        t--;
+    }
+    
+
+    return 0;
+}
