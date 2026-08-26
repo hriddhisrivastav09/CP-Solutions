a4e1350cb82b805b2d727411dc7dbed229211e84
Hriddhi Ranjan Srivastav <hriddhiranjansrivastav10b@gmail.com>
2026-08-26 23:15:40 +0530

    Add Palindrome Game (easy version) (1200)

---
+++ codeforces/1200/1527B1 - Palindrome Game (easy version).cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+using namespace std;
+using ll = long long ;
+
+ll gcd(ll a, ll b) {
+    while (b) {
+        ll t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pll = pair<ll, ll>;
+using vpll = vector<pll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define mx(v) *max_element(all(v))
+#define mn(v) *min_element(all(v))
+#define sum(v) accumulate(all(v), 0LL)
+
+#define rep(i, a, b) for(ll i = a; i < b; ++i)
+#define rrep(i, a, b) for(ll i = a; i >= b; --i)
+
+#define sz(x) ((ll)(x).size())
+#define print(x) cout << x << " " ;
+#define println(x) cout << x << '\n' ;
+#define yes cout << "Yes" << '\n';
+#define no cout << "No" << '\n';
+#define nl cout << '\n';
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+void solve(){
+
+    ll n ;
+    cin >> n ;
+
+    string s;
+    cin >> s;
+
+    bool winner = true ; 
+
+    ll cnt = 0 ;
+    rep(i,0,n){
+        if(s[i] == '0')cnt++;
+    }
+
+    if(cnt%2==0){
+        println("BOB")
+    }
+    else{
+        if(cnt==1){
+            println("BOB")
+        }
+        else{
+            println("ALICE")
+        }
+    }
+
+}
+
+
+int main() {
+
+    fast_io;
+
+    ll t;
+    cin >> t;
+    while (t > 0) {
+        solve() ;
+        t--;
+    }
+    
+
+    return 0;
+}
