d2b5083ff24a09fd986b3ba4ebcaa09d1ccc6955
Hriddhi Ranjan Srivastav <hriddhiranjansrivastav10b@gmail.com>
2026-09-02 10:35:50 +0530

    Add Friends and the Restaurant (1200)

---
+++ codeforces/1200/1729D - Friends and the Restaurant.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+using namespace std;
+using ll = long long ;
+
+ll gcd(ll a, ll b) {
+    while (b) {
+        ll t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pll = pair<ll, ll>;
+using vpll = vector<pll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define mx(v) *max_element(all(v))
+#define mn(v) *min_element(all(v))
+#define sum(v) accumulate(all(v), 0LL)
+
+#define rep(i, a, b) for(ll i = a; i < b; ++i)
+#define rrep(i, a, b) for(ll i = a; i >= b; --i)
+
+#define sz(x) ((ll)(x).size())
+#define print(x) cout << x << " " ;
+#define println(x) cout << x << '\n' ;
+#define yes cout << "Yes" << '\n';
+#define no cout << "No" << '\n';
+#define nl cout << '\n';
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+void solve(){
+
+    ll n ;
+    cin >> n ;
+
+    vll a(n);
+    rep(i , 0 , n) cin >> a[i];
+    vll b(n);
+    rep(i , 0 , n) cin >> b[i];
+
+    vll diff ;
+
+    rep(i,0,n){
+        diff.pb(b[i] - a[i]);
+    }
+
+    sort(rall(diff));
+
+    ll i = 0 ; 
+    ll j = n-1 ;
+    ll cnt = 0 ;
+
+    while(i < j){
+        if(diff[i] + diff[j] >= 0){
+            cnt++;
+            i++;
+            j--;
+        }
+        else{   
+            j--;
+        }
+    }
+
+    println(cnt)
+
+    
+
+
+}
+
+
+int main() {
+
+    fast_io;
+
+    ll t;
+    cin >> t;
+    while (t > 0) {
+        solve() ;
+        t--;
+    }
+    
+
+    return 0;
+}
