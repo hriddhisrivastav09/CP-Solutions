fbffac80ac8d3c4f497352cea304af73b346c6f8
Hriddhi Ranjan Srivastav <hriddhiranjansrivastav10b@gmail.com>
2026-09-25 16:55:42 +0530

    Add Matryoshkas (1200)

---
+++ codeforces/1200/1790D - Matryoshkas.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+using namespace std;
+using ll = long long ;
+
+ll gcd(ll a, ll b) {
+    while (b) {
+        ll t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pll = pair<ll, ll>;
+using vpll = vector<pll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define mx(v) *max_element(all(v))
+#define mn(v) *min_element(all(v))
+#define sum(v) accumulate(all(v), 0LL)
+
+#define rep(i, a, b) for(ll i = a; i < b; ++i)
+#define rrep(i, a, b) for(ll i = a; i >= b; --i)
+
+#define sz(x) ((ll)(x).size())
+#define print(x) cout << x << " " ;
+#define println(x) cout << x << '\n' ;
+#define yes cout << "Yes" << '\n';
+#define no cout << "No" << '\n';
+#define nl cout << '\n';
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+
+void solve(){
+
+    ll n ;
+    cin >> n ;
+    vll a(n);
+    rep(i , 0 , n) {
+        cin >> a[i];
+        
+    }
+
+    sort(all(a));
+
+    ll prev = 0 ;
+    ll prev_ele = mn(a) - 1;
+    ll ans = 0 ;
+    ll i = 0 ;
+    while(i<n){
+        ll curr = a[i];
+        ll j = i ;
+
+        while(j < n && a[j] == curr){
+            j++;
+        }
+
+        ll freq = j - i ;
+
+        if(curr == prev_ele+1){
+            ans += max(0LL,freq - prev);
+        }
+        else{
+            ans += freq ;
+        }
+
+        prev = freq ;
+        prev_ele = curr ;
+        i = j ;
+    }
+    
+
+    println(ans)
+
+    
+
+   
+
+
+
+}
+
+
+int main() {
+
+    fast_io;
+
+    ll t;
+    cin >> t;
+    while (t > 0) {
+        solve() ;
+        t--;
+    }
+    
+
+    return 0;
+}
